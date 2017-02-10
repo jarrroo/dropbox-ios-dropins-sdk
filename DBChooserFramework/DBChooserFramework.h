bfb26ad06edb4d84d825f6092a629986419af208
Alex Manarpies <alex@jarroo.com>
2017-02-10 12:01:37 +0100

    Carthage support

--- DBChooserFramework/DBChooserFramework.h
+++ DBChooserFramework/DBChooserFramework.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char DBChooserFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DBChooserFramework/PublicHeader.h>
 
-
+#import <DBChooserFramework/DBChooser.h>
